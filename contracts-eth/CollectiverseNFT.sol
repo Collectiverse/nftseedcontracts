6a598295945715c8da110f4d54d07fc1181d3bb2
flessner_ <dev@flessner.io>
2022-10-10 13:10:52 +0200

    all mainnet addresses

---
+++ contracts-eth/CollectiverseNFT.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+
+contract CollectiverseNFT is
+    AccessControlEnumerableUpgradeable,
+    ERC1155SupplyUpgradeable
+{
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
+    function initialize(string memory _uri) public initializer {
+        __Context_init_unchained();
+        __ERC165_init_unchained();
+        __AccessControl_init_unchained();
+        __AccessControlEnumerable_init_unchained();
+        __ERC1155_init_unchained(_uri);
+        __ERC1155Supply_init_unchained();
+        _grantRole(
+            DEFAULT_ADMIN_ROLE,
+            0x13416D5f9F25794897f40B18a0BF49a1E1633e86
+        );
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerableUpgradeable, ERC1155Upgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function mint(
+        address _owner,
+        uint256 _id,
+        uint256 _amount
+    ) external onlyRole(OPERATOR_ROLE) {
+        _mint(_owner, _id, _amount, "");
+    }
+
+    // management functions
+    function setURI(string calldata _uri)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _setURI(_uri);
+    }
+}
---
+++ contracts-eth/CollectiverseSeedSale.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./CollectiverseNFT.sol";
+
+contract CollectiverseSeedSale is Ownable, EIP712 {
+    using SafeERC20 for IERC20;
+
+    struct Voucher {
+        uint256 id;
+        uint256 price;
+        uint256[] elementIds;
+        uint256[] elementAmounts;
+        uint256 preminedId;
+        bool useWhitelist;
+        bytes signature;
+    }
+    address public signer;
+    address public immutable erc20;
+    address public wallet;
+
+    address public immutable elements;
+    address public immutable objects;
+
+    uint256 public minimumPrice;
+    uint256 public maximumPrice;
+
+    // object - element - amount
+    mapping(uint256 => mapping(uint256 => uint256)) public minable;
+
+    mapping(address => uint256) public whitelist;
+    mapping(address => uint256) public whitelistPurchased;
+
+    constructor(
+        address _elements,
+        address _objects,
+        address _signer
+    ) EIP712("CollectiverseObjects", "1") {
+        elements = _elements;
+        objects = _objects;
+        signer = _signer;
+
+        erc20 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        wallet = 0x13416D5f9F25794897f40B18a0BF49a1E1633e86;
+        _transferOwnership(0x13416D5f9F25794897f40B18a0BF49a1E1633e86);
+
+        // 50 usdc
+        minimumPrice = 50000000;
+
+        // 2000 usdc
+        maximumPrice = 5000000000;
+    }
+
+    // minting objects
+    function mintObject(address _owner, Voucher calldata _voucher)
+        external
+        returns (uint256)
+    {
+        uint256 _id = _voucher.id;
+        uint256 _pre = _voucher.preminedId;
+
+        // checking the signature, voucher & whitelist
+        address voucherSigner = _verify(_voucher);
+        require(signer == voucherSigner, "voucher is not signed by the signer");
+
+        require(CollectiverseNFT(objects).totalSupply(_id) < 1, "exist alredy");
+
+        if (_voucher.useWhitelist) {
+            whitelistPurchased[_owner] += 1;
+            require(
+                (whitelistPurchased[_owner]) <= whitelist[_owner],
+                "whitelist limit"
+            );
+        }
+
+        // payment, elements, premining & minting
+        require(_voucher.price >= minimumPrice, "not minimum price");
+        require(_voucher.price <= maximumPrice, "not maximum price");
+        IERC20(erc20).safeTransferFrom(msg.sender, wallet, _voucher.price);
+
+        require(_voucher.elementIds.length == _voucher.elementAmounts.length);
+        for (uint256 i = 0; i < _voucher.elementIds.length; i++) {
+            minable[_id][_voucher.elementIds[i]] = _voucher.elementAmounts[i];
+        }
+
+        CollectiverseNFT(elements).mint(_owner, _pre, minable[_id][_pre]);
+        minable[_id][_pre] = 0;
+
+        CollectiverseNFT(objects).mint(_owner, _id, 1);
+        return _id;
+    }
+
+    // management functions
+    function setSettings(
+        address _signer,
+        address _wallet,
+        uint256 _minimumPrice,
+        uint256 _maximumPrice
+    ) external onlyOwner {
+        signer = _signer;
+        wallet = _wallet;
+        minimumPrice = _minimumPrice;
+        maximumPrice = _maximumPrice;
+    }
+
+    function whitelistAddresses(address[] memory _addresses, uint256 _amount)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            whitelist[_addresses[i]] = _amount;
+        }
+    }
+
+    // EIP 712
+    function _hash(Voucher calldata voucher) internal view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "Voucher(uint256 id,uint256 price,uint256[] elementIds,uint256[] elementAmounts,uint256 preminedId,bool useWhitelist)"
+                        ),
+                        voucher.id,
+                        voucher.price,
+                        keccak256(abi.encodePacked(voucher.elementIds)),
+                        keccak256(abi.encodePacked(voucher.elementAmounts)),
+                        voucher.preminedId,
+                        voucher.useWhitelist
+                    )
+                )
+            );
+    }
+
+    function _verify(Voucher calldata voucher) internal view returns (address) {
+        bytes32 digest = _hash(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+}
